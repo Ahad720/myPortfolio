ead5d6ce3ad66a4f36b69aa0d34bd15c48412a47
Abdul Ahad Khan <167075636+AbdulAhadKhanTL@users.noreply.github.com>
2024-05-09 15:05:29 +0100

    profile updated

--- lib/core/animations/zoom_animation.dart
+++ lib/core/animations/zoom_animation.dart
@@ -92,7 +92,7 @@ class _ZoomAnimationsState extends State<ZoomAnimations>
               image: const DecorationImage(
                 fit: BoxFit.cover,
                 alignment: Alignment.bottomLeft,
-                image: AssetImage('assets/imgs/IMG_1035.png'),
+                image: AssetImage('assets/imgs/profile.png'),
               ),
             ),
           ),
